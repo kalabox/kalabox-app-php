711bcfceaa80cdee4d4d34275f97ee01035a3e89
Mike Pirog <mike@kalamuna.com>
2016-06-28 12:37:21 -0700

    #1409: Fix build to include plugin entrypoint

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,10 +39,11 @@ module.exports = function(grunt) {
     copy: {
       app: {
         src: [
-          'package.json',
-          'lib/**/*',
+          'index.js',
+          'lib/*.js',
           'app/**/*',
-          'app/.gitignore'
+          'app/.gitignore',
+          'package.json'
         ],
         dest: 'build',
         expand: true,
